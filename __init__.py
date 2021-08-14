46a5f57cc83bae88dc92cba74bb35659e6c66867
jarbasal <jarbasai@mailfence.com>
2021-08-14 18:20:15 +0100

    common playing

--- __init__.py
+++ __init__.py
@@ -5,7 +5,8 @@ from mycroft.skills.core import intent_handler
 from mycroft.messagebus.message import Message
 from ovos_workshop.skills import OVOSSkill
 from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
-    CommonPlayMatchConfidence, OVOSCommonPlaybackInterface, CommonPlayStatus
+    CommonPlayMatchConfidence, OVOSCommonPlaybackInterface, \
+    CommonPlayStatus, VideoPlayerType, AudioPlayerType
 from ovos_workshop.frameworks.playback.youtube import is_youtube, \
     get_youtube_metadata, \
     get_youtube_video_stream
@@ -13,6 +14,7 @@ from ovos_utils.gui import is_gui_connected, GUIInterface
 from ovos_utils.json_helper import merge_dict
 from ovos_utils.log import LOG
 from pprint import pprint
+from ovos_workshop.frameworks.playback.playlists import Playlist
 
 
 class BetterPlaybackControlSkill(OVOSSkill):
@@ -21,20 +23,27 @@ class BetterPlaybackControlSkill(OVOSSkill):
         # TODO skill settings for these values
         self.gui_only = False  # not recommended
         self.audio_only = False
+        self.use_mycroft_gui = False  # send all playback to the plugin
         self.compatibility_mode = True
         self.media_type_fallback = True # if True send a Generic type query
         # when specific query fails, eg "play the News" -> news media not
         # found -> check other skills (youtube/iptv....)
-        self.cps = OVOSCommonPlaybackInterface(
+        if self.self.use_mycroft_gui:
+            audio = AudioPlayerType.MYCROFT
+            video = VideoPlayerType.MYCROFT
+        else:
+            audio = AudioPlayerType.SIMPLE
+            video = VideoPlayerType.SIMPLE
+        self.common_play = OVOSCommonPlaybackInterface(
             bus=self.bus, backwards_compatibility=self.compatibility_mode,
-            media_fallback=self.media_type_fallback,
-            max_timeout=7, min_timeout=3)
+            media_fallback=self.media_type_fallback, audio_player=audio,
+            video_player=video, max_timeout=7, min_timeout=3)
 
-        self.add_event("better_cps.play", self.handle_play_request)
+        self.add_event("ovos.common_play.play", self.handle_play_request)
 
     def stop(self, message=None):
-        # will stop any playback in GUI or AudioService
-        return self.cps.stop()
+        # will stop any playback in VIDEO or AudioService
+        return self.common_play.stop()
 
     # play xxx intents
     @intent_handler("play.intent")
@@ -161,27 +170,27 @@ class BetterPlaybackControlSkill(OVOSSkill):
     @intent_handler(IntentBuilder('NextCommonPlay')
                     .require('Next').require("Playing").optionally("Track"))
     def handle_next(self, message):
-        self.cps.play_next()
+        self.common_play.play_next()
 
     @intent_handler(IntentBuilder('PrevCommonPlay')
                     .require('Prev').require("Playing").optionally("Track"))
     def handle_prev(self, message):
-        self.cps.play_prev()
+        self.common_play.play_prev()
 
     @intent_handler(IntentBuilder('PauseCommonPlay')
                     .require('Pause').require("Playing"))
     def handle_pause(self, message):
-        self.cps.pause()
+        self.common_play.pause()
 
     @intent_handler(IntentBuilder('ResumeCommonPlay')
                     .one_of('PlayResume', 'Resume').require("Playing"))
     def handle_resume(self, message):
         """Resume playback if paused"""
-        self.cps.resume()
+        self.common_play.resume()
 
     # playback selection
     def should_resume(self, phrase):
-        if self.cps.playback_status == CommonPlayStatus.PAUSED:
+        if self.common_play.playback_status == CommonPlayStatus.PAUSED:
             if not phrase.strip() or \
                     self.voc_match(phrase, "Resume", exact=True) or \
                     self.voc_match(phrase, "Play", exact=True):
@@ -196,23 +205,26 @@ class BetterPlaybackControlSkill(OVOSSkill):
 
         # if media is currently paused, empty string means "resume playback"
         if self.should_resume(phrase):
-            self.cps.resume()
+            self.common_play.resume()
             return
 
         self.speak_dialog("just.one.moment", wait=True)
 
         self.enclosure.mouth_think()
 
+        # reset common play playlist
+        self.common_play.playlist = Playlist()
+
         # Now we place a query on the messsagebus for anyone who wants to
         # attempt to service a 'play.request' message.
         results = []
-        for r in self.cps.search(phrase, media_type=media_type):
+        for r in self.common_play.search(phrase, media_type=media_type):
             results += r["results"]
 
-        # filter GUI only results if GUI not connected
+        # filter VIDEO only results if VIDEO not connected
         gui_connected = is_gui_connected(self.bus)
         if self.audio_only or not gui_connected:
-            results = [r for r in results if r["playback"] != CommonPlayPlaybackType.GUI]
+            results = [r for r in results if r["playback"] != CommonPlayPlaybackType.VIDEO]
 
         if not results:
             self.speak_dialog("cant.play",
@@ -221,7 +233,7 @@ class BetterPlaybackControlSkill(OVOSSkill):
             return
 
         best = self.select_best(results)
-        self.cps.play_media(best, results)
+        self.common_play.play_media(best, results)
         self.enclosure.mouth_reset()
         self.set_context("Playing")
 
@@ -242,11 +254,11 @@ class BetterPlaybackControlSkill(OVOSSkill):
             selected = random.choice(ties)
 
             if self.gui_only:
-                # select only from GUI results if preference is set
+                # select only from VIDEO results if preference is set
                 # WARNING this can effectively make it so that the same
                 # skill is always selected
                 gui_results = [r for r in ties if r["playback"] ==
-                               CommonPlayPlaybackType.GUI]
+                               CommonPlayPlaybackType.VIDEO]
                 if len(gui_results):
                     selected = random.choice(gui_results)
 
@@ -267,10 +279,10 @@ class BetterPlaybackControlSkill(OVOSSkill):
             media = message.data.get("media")
             playlist = message.data.get("playlist") or [media]
             disambiguation = message.data.get("disambiguation") or [media]
-        self.cps.play_media(media, disambiguation, playlist)
+        self.common_play.play_media(media, disambiguation, playlist)
 
     def shutdown(self):
-        self.cps.shutdown()
+        self.common_play.shutdown()
 
 
 def create_skill():
