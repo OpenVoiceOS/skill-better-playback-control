71ca166cdd4262973c3f93d4a24ee3ede6598695
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-12-08 18:48:28 +0000

    :tada: - packaging (#7)
    * :tada: - packaging
    
    * add MANIFEST.in
    
    authored-by: jarbasai <jarbasai@mailfence.com>

--- __init__.py
+++ __init__.py
@@ -5,6 +5,8 @@ from ovos_workshop.skills import OVOSSkill
 class OCPSkill(OVOSSkill):
 
     def initialize(self):
+        # TODO setup the plugin in mycroft.conf if needed
+        # allows distributing OCP via skill stores
         LOG.info("This skill has been replaced with a plugin, "
                  "please see https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin")
 
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+# skill_id=package_name:SkillClass
+PLUGIN_ENTRY_POINT = 'mycroft-playback-control.mycroftai=ovos_skill_common_play:OCPSkill'
+# in this case the skill_id is defined to purposefully replace the mycroft version of the skill,
+# or rather to be replaced by it in case it is present. all skill directories take precedence over plugin skills
+
+
+setup(
+    # this is the package name that goes on pip
+    name='ovos-skill-common-play',
+    version='0.0.1',
+    description='OVOS common-play skill plugin',
+    url='https://github.com/OpenVoiceOS/skill-ovos-common-play',
+    author='JarbasAi',
+    author_email='jarbasai@mailfence.com',
+    license='Apache-2.0',
+    package_dir={"ovos_skill_common_play": ""},
+    packages=['ovos_skill_common_play'],
+    include_package_data=True,
+    install_requires=["ovos-plugin-manager>=0.0.2",
+                      "ovos_plugin_common_play>=0.0.1a2"],
+    keywords='ovos skill plugin',
+    entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
+)
